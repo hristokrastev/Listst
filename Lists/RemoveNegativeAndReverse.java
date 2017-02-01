2952000d4a49538bf8cd3c76266ac84cf40af07b
hristokrastev <hristo.krastevii@gmail.com>
2017-02-01 22:50:19 +0200

    Updating the created live templates
    Templates for Reading as String, Integer, Double, and printings without [].

--- Lists/RemoveNegativeAndReverse.java
+++ Lists/RemoveNegativeAndReverse.java
@@ -1,9 +1,6 @@
 package Java_fund_second_couse.Lists;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -12,26 +9,36 @@ public class RemoveNegativeAndReverse {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
-        List<Integer> list =
-                IntStream.of(Arrays.stream(
-                        console.nextLine()
-                                .split("\\s+"))
-                        .mapToInt(Integer::parseInt)
-                        .toArray())
-                        .boxed()
-                        .collect(Collectors.toList());
+        // Reading String
+//        List<String> listOfElements =
+//                (Arrays.asList(
+//                        console.nextLine()
+//                                .split("\\s+")));
 
+        //Reading as INT
+//        List<Integer> list =
+//                IntStream.of(Arrays.stream(
+//                        console.nextLine()
+//                                .split("\\s+"))
+//                        .mapToInt(Integer::parseInt)
+//                        .toArray())
+//                        .boxed()
+//                        .collect(Collectors.toList());
+//
+//
+//        ArrayList<Integer> result = new ArrayList<Integer>();
+//
+//
+        // Printing the ArrayList
+//        for (Integer item : list) {
+//            if (item > 0) {
+//                result.add(item);
+//            }
+//        }
+//
+        // Printing without "[" "]" on one row
+//            System.out.println(result.toString().substring(1, result.toString().length()-1));
 
-        ArrayList<Integer> result = new ArrayList<Integer>();
-
-
-        for (Integer item : list) {
-            if (item > 0) {
-                result.add(item);
-            }
-        }
-
-            System.out.println(result.toString().substring(1, result.toString().length()-1));
 
 
 //
